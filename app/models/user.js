cd41d808b83fd749ab2ea99a64a593f9381cfc0a
colebuildanddevelop <colebuildanddevelop@gmail.com>
2020-10-22 02:00:42 -0500

    errors displaying

--- app/models/user.js
+++ app/models/user.js
@@ -1,21 +1,31 @@
 const mongoose = require("mongoose");
+var uniqueValidator = require("mongoose-unique-validator");
 const Schema = mongoose.Schema;
 
-const UserSchema = new Schema({
+const userSchema = new Schema({
   username: { type: String, unique: true },
-  email: String,
+  email: { type: String, unique: true },
   password: String,
-  avatar: { type: String, default: "https://img.favpng.com/3/11/25/avatar-youtube-cat-png-favpng-PRA6iZsrgWAqXFqmjMsfSvuPG.jpg" },
+  avatar: {
+    type: String,
+    default:
+      "https://img.favpng.com/3/11/25/avatar-youtube-cat-png-favpng-PRA6iZsrgWAqXFqmjMsfSvuPG.jpg",
+  },
   accountBalance: { type: Number, default: 10000 },
-  wins: {type: Number, default: 0},
-  friends: [{
-    type: Schema.Types.ObjectId,
-    ref: "User"
-  }],
-  entries: [{
-    type: Schema.Types.ObjectId, 
-    ref: "Entry"
-  }]
+  wins: { type: Number, default: 0 },
+  friends: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  entries: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Entry",
+    },
+  ],
 });
 
-module.exports = mongoose.model("User", UserSchema);
+userSchema.plugin(uniqueValidator);
+module.exports = mongoose.model("User", userSchema);
